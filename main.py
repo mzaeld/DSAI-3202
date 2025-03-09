df9de444a2cccdc342055cda0f46a83fed05a2aa
Amelda Zahira <60302323@udst.edu.qa>
2025-03-09 12:05:03 +0300

    more examples

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
 from mpi4py import MPI
+import numpy as np
+from src.square import square
+import time
+import random
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
+size = comm.Get_size() #number of processes
+results = None
+print(f'Which process is this {rank}, and the size is {size}')
 
 if rank == 0:
-    data = {'a': 7, 'b': 3.14}  # Fixed quotes
-    comm.send(data, dest=1, tag=11)
-    print('On process 0, data sending:', data)
-elif rank == 1:
-    data = comm.recv(source=0, tag=11)
-    print('On process 1, data received:', data)  # Fixed print statement
+    numbers = np.arange(size,dtype = 'i')
+else: 
+    numbers = None
+print(numbers)
+
+number = np.zeros(1,dtype = 'i') #creates vectors of zeros with dimension of 1
+comm.Scatter(numbers,number,root = 0) # distributing the numbers to all the processes
+print(numbers)
+print(number)
+
+result = square(number[0])
+print(result)
+time.sleep(random.randint(1,10))
+request = comm.isend(result,dest=0,tag = rank)
+
+
+if rank == 0:
+    results = np.zeros(size,dtype = 'i')
+    for i in range(size):
+        results[i] = comm.irecv(source = i,tag = i).wait()
+    print(f'The results are: {results}')
+    
+request.wait()
+
+        
+
+
+
+
+
 
